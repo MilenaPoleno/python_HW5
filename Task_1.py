46b1a29bf03008cc4efcd64596c4e4f8381d5274
Milena <emilieguryeva@gmail.com>
2022-12-10 11:57:36 +0300

    add Task_1

---
+++ Task_1.py
@@ -0,0 +1,18 @@
+"""Создать программно файл в текстовом
+формате, записать в него построчно
+данные, вводимые пользователем. Об
+окончании ввода данных
+свидетельствует пустая строка."""
+
+with open("user_data.txt", "w",
+                    encoding="utf-8") as f_obj:
+    user_line = input("Введите данные: ")
+    while user_line:
+        f_obj.write(f"{user_line} ")
+        user_line = input("Введите данные: ")
+        if not user_line:
+            break
+
+with open("user_data.txt", 'r',
+                    encoding="utf-8") as f_obj:
+    print(f_obj.readlines())
