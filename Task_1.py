88a4aaa78c6d17ce57e67d1edc6e7f93d03cc7e6
Milena <emilieguryeva@gmail.com>
2022-12-10 12:40:03 +0300

    add Task_1

--- Task_1.py
+++ Task_1.py
@@ -8,7 +8,7 @@ with open("user_data.txt", "w",
                     encoding="utf-8") as f_obj:
     user_line = input("Введите данные: ")
     while user_line:
-        f_obj.write(f"{user_line} ")
+        f_obj.writelines(f"{user_line} \n")
         user_line = input("Введите данные: ")
         if not user_line:
             break
